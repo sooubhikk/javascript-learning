103ad0e5ebdf783a025808307360b44d7086d9cb
soubhik karmakar <soubhikkarmakar807@gmail.com>
2024-07-03 14:06:34 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser..in browser we can use it
+
+// code readability must be high
+
+let name = "soubhik" // string
+let age = 21 // integer
+let isLoggedin = false // boolean
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean
+//null => standalone value
+//undefined 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // type is undefined 
+console.log(typeof null); // type is object
